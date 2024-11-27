75d089f37c04eabc16a0342630ca58bf3724e73e
Pranav <mpranavprem@gmail.com>
2024-11-27 12:34:01 +0530

    changes

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import "./Cart.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   return (
-    <div class="page-wrapper tailwind">
+    <div class="page-wrapper">
       <Header />
-      <h1 className="text-3xl font-bold underline">Cart</h1>
+      <div class="main-container">
+        <div class="inner-banner thm-black-bg text-center">
+          <div class="container">
+            <h2 class="inner-banner__title">My Cart</h2>
+            <ul class="thm-breadcrumb">
+              <li class="thm-breadcrumb__item">
+                <Link to="/">Home</Link>
+              </li>
+              <li class="thm-breadcrumb__item">
+                <span>My Cart</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="container">
+          <h1>My Cart</h1>
+        </div>
+      </div>
       <Footer />
     </div>
   );
