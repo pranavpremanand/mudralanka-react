e9f04712fc04a5d2e4b7c8b3dd9d47996e1abbdc
Pranav <mpranavprem@gmail.com>
2024-11-28 23:21:08 +0530

    changed email

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 // send email link
 export const sendEmailLink =
-  "http://localhost:5000/api/send-email";
-  // "https://mudralanka-sendmail.vercel.app/api/send-email";
+  // "http://localhost:5000/api/send-email";
+  "https://mudralanka-sendmail.vercel.app/api/send-email";
 
 // Categories
 export const categories = [
